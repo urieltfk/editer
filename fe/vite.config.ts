23dd3c6b11a4ebe7421dd988346c5a752b88462d
Uriel Gaimani <urieltfk@gmail.com>
2025-10-17 20:19:14 +0300

    fixed build errors

--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -1,18 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@routes': path.resolve(__dirname, './src/routes'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@lib': path.resolve(__dirname, './src/lib'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@components': path.resolve(__dirname, './src/components'),
+      '@': '/src',
+      '@routes': '/src/routes',
+      '@pages': '/src/pages',
+      '@lib': '/src/lib',
+      '@assets': '/src/assets',
+      '@components': '/src/components',
     },
   },
   server: {
